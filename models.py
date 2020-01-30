da97c1e456ea5a703cd41f360ebed2e56e112d58
Ryen Krusinga <krusinga@cs.umd.edu>
2020-01-29 19:50:59 -0500

    Saving

--- models.py
+++ models.py
@@ -16,7 +16,8 @@ from dataloader import stat_grid
 # Should I have max pools like alexnet?
 class OurRewardPredictor(nn.Module):
     def __init__(self, loadmodel=False, loadfolder=L2DATA, experiment_name='experiment1', 
-                model_name="PongRewardsEpoch100", color_adversary=False, gpuid=0, loadname=None):
+                model_name="PongRewardsEpoch100", color_adversary=False, paddle_predictor=False,
+                gpuid=0, loadname=None):
         super().__init__()
 
         self.features = nn.Sequential(
@@ -61,6 +62,18 @@ class OurRewardPredictor(nn.Module):
             self.color_discriminator = None
             self.disc_optimizer = None
 
+        if paddle_predictor:
+            self.paddle_predictor = nn.Sequential(
+                    nn.Dropout(), #?
+                    nn.Linear(256, 256),
+                    nn.ReLU(inplace=True),
+                    nn.Linear(256,1) #Just 1 for now, binary colors
+                    )
+        else:
+                self.paddle_predictor=None
+                self.paddle_optimizer=None
+
+
         if loadmodel:
             if loadname is None:
                 loadname = model_name
@@ -73,6 +86,8 @@ class OurRewardPredictor(nn.Module):
                 self.reward_classifier = d['_modules']['reward_classifier']
                 if 'color_discriminator' in d['_modules']:
                     self.color_discriminator = d['_modules']['color_discriminator']
+                if 'paddle_predictor' in d['_modules']:
+                    self.paddle_predictor = d['_modules']['paddle_predictor']
                 #self.__dict__.update(d)
 
 #        self.experiment_name = experiment_name
@@ -82,16 +97,23 @@ class OurRewardPredictor(nn.Module):
         self.current_input_spaces = None
         self.current_output_spaces = None
         self.color_adversary = color_adversary
+        self.paddle_predictor = paddle_predictor
 
         # This code creates new optimizers instead of using saved ones. Good or bad?
         self.device = torch.device("cuda:{}".format(gpuid) if torch.cuda.is_available() else "cpu")
         self.features = self.features.to(self.device)
         self.reward_classifier = self.reward_classifier.to(self.device)
-        self.main_optimizer = optim.Adam(chain(self.features.parameters(), self.reward_classifier.parameters()), 
+        self.main_optimizer = optim.Adam(chain(self.features.parameters(), 
+                                         self.reward_classifier.parameters()), 
                                          lr=0.0002)
         if self.color_adversary:
             self.color_discriminator = self.color_discriminator.to(self.device)    
             self.disc_optimizer = optim.Adam(self.color_discriminator.parameters(), lr=0.0002)
+
+        if self.paddle_predictor:
+            self.paddle_predictor = self.paddle_predictor.to(self.device)
+            self.paddle_optimizer = optim.Adam(self.paddle_predictor.parameters()), lr=0.0002)
+
 
 
     # These don't affect the rest of the model currently
@@ -123,8 +145,21 @@ class OurRewardPredictor(nn.Module):
             cdetached = None
             c = None
 
-        y_hat = { 'reward_pred': y, 'color_pred': c, 'adversary_pred':cdetached}
-#                  'reward': y.detach().cpu().numpy()  }
+        if self.paddle_predictor:
+            z = x.detach()
+            pdetached = self.paddle_predictor(z).squeeze()
+            p = self.paddle_predictor(x).squeeze()
+        else:
+            pdetached = None
+            p = None
+
+
+        y_hat = { 'reward_pred': y, 
+                  'color_pred': c, 
+                  'adversary_pred':cdetached,
+                  'paddle_pred':p,
+                  'paddle_detached_pred':pdetached } 
+
 
         return y_hat #Question: should I make discriminator separate,
                                     # and update its weights before redoing calculation?
@@ -166,7 +201,7 @@ class OurRewardPredictor(nn.Module):
 # assume 3x32x32
 class OurSimpleRewardPredictor(nn.Module):
     def __init__(self, loadmodel=False, loadfolder=L2DATA, experiment_name='experiment1', 
-                model_name="PongRewardsEpoch100", color_adversary=False, gpuid=0, loadname=None):
+                model_name="PongRewardsEpoch100", color_adversary=False, paddle_predictor=False, gpuid=0, loadname=None):
         super().__init__()
         print("Simple reward predictor")
 
@@ -212,12 +247,22 @@ class OurSimpleRewardPredictor(nn.Module):
                 self.color_discriminator = None
                 self.disc_optimizer = None
 
+            if paddle_predictor:
+                self.paddle_predictor = nn.Sequential(
+                        nn.Dropout(), #?
+                        nn.Linear(256, 256),
+                        nn.ReLU(inplace=True),
+                        nn.Linear(256,1) #Just 1 for now, binary colors
+                        )
+
+
         self.experiment_name = experiment_name
         self.model_name = model_name
         self.task_spaces = None
         self.current_input_spaces = None
         self.current_output_spaces = None
         self.color_adversary = color_adversary
+        self.paddle_predictor = paddle_predictor
 
         self.device = torch.device("cuda:{}".format(gpuid) if torch.cuda.is_available() else "cpu")
         self.features = self.features.to(self.device)
@@ -227,6 +272,10 @@ class OurSimpleRewardPredictor(nn.Module):
         if self.color_adversary:
             self.color_discriminator = self.color_discriminator.to(self.device)
             self.disc_optimizer = optim.Adam(self.color_discriminator.parameters(), lr=0.0002)
+
+        if self.paddle_predictor:
+            self.paddle_predictor = self.paddle_predictor.to(self.device)
+            self.paddle_optimizer = optim.Adam(self.paddle_predictor.parameters()), lr=0.0002)
            
 
 
@@ -260,7 +309,17 @@ class OurSimpleRewardPredictor(nn.Module):
             cdetached = None
             c = None
 
-        y_hat = { 'reward_pred': y, 'color_pred': c, 'adversary_pred':cdetached}
+        if self.paddle_predictor:
+            z = x.detach()
+            pdetached = self.paddle_predictor(z).squeeze()
+            p = self.paddle_predictor(x).squeeze()
+        else:
+            pdetached=None
+            p=None
+
+        y_hat = { 'reward_pred': y, 'color_pred': c, 'adversary_pred':cdetached, 
+                    'paddle_pred':p,
+                    'paddle_detached_pred':pdetached } # Not sure if detached is necessary here
 #                  'reward': y.detach().cpu().numpy()  }
 
         return y_hat #Question: should I make discriminator separate,
@@ -296,34 +355,12 @@ class OurSimpleRewardPredictor(nn.Module):
         pickle.dump(self.__dict__, f, 2) #pickle with protocol 2 for efficiency
         f.close()       
 
+# "Extrapolation networks" idea - exploit linearity of sub-attributes like color
+
 class OurOptimizer:
     def __init__(self, model, lmbda=1):
         self.device = model.device
         self.lmbda = lmbda
-#        self.stats = dataset.statistics()
-
-#    def calculate_loss(self, y, y_hat): #y and y_hat should be dicts of all info
-#        reward_loss = nn.functional.binary_cross_entropy_with_logits(y_hat['reward_loss_pred'], 
-#                        torch.Tensor(y['reward']).to(self.device))
-#
-#        if y_hat['color_loss_pred'] is not None and y_hat['adversary_loss_pred'] is not None:
-#            color_loss = nn.functional.binary_cross_entropy_with_logits(y_hat['color_loss_pred'], 
-#                            1-torch.Tensor(y['color']))
-#            adversary_loss = nn.functional.binary_cross_entropy_with_logits(y_hat['adversary_loss_pred'], 
-#                            torch.Tensor(y['color']).to(self.device))
-#        else:
-#            color_loss = 0
-#            adversary_loss = None
-#
-#        reward_loss = reward_loss+self.lmbda*color_loss
-#
-#        loss = {'reward_loss': reward_loss, 'adversary_loss': adversary_loss}
-#                 #'reward': reward_loss_grad.detach().numpy()} # numpy for benefit of TEF
-#        return loss
-        # loss y['reward'], y_hat['reward']
-        # loss y['color'], y_hat['color'] (negative of adversary loss)
-        # loss y['adversary'], y_hat['adversary']
-        # add first two losses together with some constant coefficient
 
     def calculate_loss(self, y, y_hat):
         reward_predictions = y_hat['reward_pred']
@@ -338,11 +375,6 @@ class OurOptimizer:
         reward_acc = reward_num_correct / len(reward_actual)
 
         sumgrid, totalgrid, proportiongrid = stat_grid(torch.sigmoid(reward_predictions), size_actual, color_actual)
-
-        # Debugging purposes
-#        if any(torch.isnan(proportiongrid.flatten())):
-#            print("Nans!")
-
 
         if color_predictions is not None:
             color_loss = nn.functional.binary_cross_entropy_with_logits(color_predictions, 1-color_actual)
@@ -372,6 +404,70 @@ class OurOptimizer:
 
         return losses
 
+
+class OptWithPaddleLoss:
+    def __init__(self, model, lmbda=[1,1]):
+        self.device = model.device
+        self.lmbda = lmbda
+
+    def calculate_loss(self, y, y_hat):
+        reward_predictions = y_hat['reward_pred']
+        color_predictions = y_hat['color_pred']
+        paddle_predictions = y_hat['paddle_pred']
+        paddle_detached_predictions = y_hat['paddle_detached_pred']
+        adversary_predictions = y_hat['adversary_pred']
+
+        reward_actual = torch.Tensor(y['reward']).to(self.device)
+        color_actual = torch.Tensor(y['bg_color']).to(self.device)
+        paddle_bot_actual = torch.Tensor(y['bot/paddle/width']).to(self.device)
+        paddle_agent_actual = torch.Tensor(y['agent/paddle/width']).to(self.device)
+
+        reward_loss = nn.functional.binary_cross_entropy_with_logits(reward_predictions, reward_actual)
+        reward_num_correct = ((reward_predictions > 0).int() == reward_actual.int()).sum().float()
+        reward_acc = reward_num_correct / len(reward_actual)
+
+        if paddle_predictions is not None:
+            pass #which loss to use, if cts?
+        else:
+            paddle_loss = 0
+
+        if paddle_detached_predictions is not None:
+            pass
+        else:
+            paddle_detached_loss = None
+            paddle_detached_rmse = None
+
+
+        if color_predictions is not None:
+            color_loss = nn.functional.binary_cross_entropy_with_logits(color_predictions, 1-color_actual)
+        else:
+            color_loss = 0
+
+        if adversary_predictions is not None:
+            adversary_loss = nn.functional.binary_cross_entropy_with_logits(adversary_predictions, color_actual)
+            adversary_num_correct = ((adversary_predictions > 0).int() == color_actual.int()).sum().float()
+            adversary_acc = adversary_num_correct / len(color_actual)
+        else:
+            adversary_loss = None
+            adversary_acc = None
+            adversary_num_correct = None
+
+        reward_loss = reward_loss+self.lmbda[0]*color_loss+self.lmbda[1]*paddle_loss
+
+        losses = { 'reward_loss': reward_loss,
+                   'adversary_loss': adversary_loss,
+                   'paddle_detached_loss': paddle_detached_loss,
+                   'paddle_detached_rmse': paddle_detached_rmse,
+                   'reward_acc': reward_acc,
+                   'reward_num_correct': reward_num_correct,
+                   'adversary_acc': adversary_acc,
+                   'adversary_num_correct': adversary_num_correct,
+                   'sumgrid': None,
+                   'totalgrid': None,
+                   'proportiongrid': None,
+                   }
+
+        return losses
 
 
         
--- train.py
+++ train.py
@@ -14,12 +14,15 @@ from torch.utils.tensorboard import SummaryWriter
 import time
 
 
-def train_model(mclass, train_syllabus, test_syllabus, nepochs, model_name, use_adversary, gpu=0,
-                resize=None, noise=None, loadname=None, loadmodel=False, cutoff=None):
+def train_model(mclass, train_syllabus, test_syllabus, 
+                nepochs, model_name, use_adversary, 
+                OptClass=OurOptimizer, gpu=0,
+                resize=None, noise=None, loadname=None, 
+                loadmodel=False, cutoff=None):
     exp_name = os.path.basename(os.path.splitext(train_syllabus)[0])
     model = mclass(loadmodel=loadmodel, experiment_name=exp_name, model_name=model_name,
                                 color_adversary=use_adversary, gpuid=gpu, loadname=loadname)
-    optim = OurOptimizer(model)
+    optim = OptClass(model)
 
 #    totalset = L2M_Pytorch_Dataset(syllabus)
 #    train_length = int(0.8*len(totalset))
@@ -127,6 +130,7 @@ if __name__ == '__main__':
     parser.add_argument('--noise', type=float, default=None)
     parser.add_argument('--loadname', default=None)
     parser.add_argument('--cutoff', type=float, default=None)
+    parser.add_argument('--opt_class', default='OurOptimizer')
    
     args = parser.parse_args()
     
@@ -135,18 +139,26 @@ if __name__ == '__main__':
     if args.model_class == 'OurSimpleRewardPredictor':
         mclass = OurSimpleRewardPredictor
         resize = 32
+
+    OptClass = OurOptimizer
+    if args.opt_class == 'OptWithPaddleLoss':
+        OptClass = OptWithPaddleLoss
         
     loadmodel = False
     if args.loadname is not None:
         loadmodel=True
     
     torch.manual_seed(args.random_seed)
+
+
+    #   Need to add in kwarg syntax here to not confuse position
     train_model(mclass,
                 module_relative_file(__file__, args.train_syllabus), 
                 module_relative_file(__file__, args.test_syllabus),
                 args.nepochs, 
                 args.model_name, 
                 args.use_adversary,
+                OptClass,
                 args.gpuid,
                 resize,
                 args.noise,
